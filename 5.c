14ca1f9212dbf66ab9dbfb28ac2efdb3a8184af1
C-137 <24hours.forgame@gmail.com>
2022-03-31 20:59:20 +0700

    Revert "Initial commit"
    This reverts commit 521e17725a8ce48c71b66f42d2cff048a16a0b3c.

--- 5.c
+++
@@ -1,8 +0,0 @@
-//write a program to compare two number by using of ternary operator in c
-
-#include <stdio.h> 
-int main() 
-{ 
-        int x=8, y=4 ; 
-        y = ( x>y ? printf("x is greater") : printf("y is greater") ) ; 
-}
